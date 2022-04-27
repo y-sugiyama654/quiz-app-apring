7b52bdad3524f9249d715d3fa083e1f78c42d104
Yuta <y.sugiyama54@gmail.com>
2022-04-27 18:46:35 +0900

    データ全件取得処理の実装

--- src/main/java/com/example/quiz/QuizApplication.java
+++ src/main/java/com/example/quiz/QuizApplication.java
@@ -26,12 +26,18 @@ public class QuizApplication {
 	private void execute() {
 		// 登録処理
 		setup();
+		// 全件取得
+		showList();
 	}
 	
 	/**
-	 * クイズを2件作成s 
+	 * クイズを2件作成
 	 */
 	private void setup() {
+		
+		// データの全件削除処理
+		repository.deleteAll();
+		
 		// エンティティの生成
 		Quiz quiz1 = new Quiz(null, "「Spring」はフレームワークか？", true, "登録太郎");
 		// 登録処理
@@ -43,6 +49,18 @@ public class QuizApplication {
 		// 登録処理
 		quiz2 = repository.save(quiz2);
 		System.out.println("登録したデータは、" + quiz2 + "です。");
+	}
+	
+	/**
+	 * 全件取得
+	 */
+	private void showList() {
+		System.out.println("-----全件取得処理の開始-----");
+		Iterable<Quiz> quizzes = repository.findAll();
+		for (Quiz quiz: quizzes) {
+			System.out.println(quiz);
+		}
+		System.out.println("-----全件取得処理の完了-----");
 	}
 
 }
