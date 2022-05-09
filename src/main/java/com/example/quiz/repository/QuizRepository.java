aa09c2a66262816319f479c268e12f079810e0ad
Yuta <y.sugiyama54@gmail.com>
2022-05-09 22:50:42 +0900

    クイズをランダムで1件取得処理の追加

--- src/main/java/com/example/quiz/repository/QuizRepository.java
+++ src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -1,5 +1,6 @@
 package com.example.quiz.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.quiz.entity.Quiz;
@@ -8,5 +9,8 @@ import com.example.quiz.entity.Quiz;
  * Quizテーブル：RepositoryImpl
  */
 public interface QuizRepository extends CrudRepository<Quiz, Integer>{
+	
+	@Query("SELECT id FROM quiz ORDER BY RANDOM() limit 1")
+	Integer getRandomId();
 
 }
