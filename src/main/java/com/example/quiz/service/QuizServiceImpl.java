8b7de6d3a189a3393bd4cd2d648e05abfe8f3a62
Yuta <y.sugiyama54@gmail.com>
2022-05-09 22:51:06 +0900

    サービス処理の追加

---
+++ src/main/java/com/example/quiz/service/QuizServiceImpl.java
@@ -0,0 +1,74 @@
+package com.example.quiz.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.quiz.entity.Quiz;
+import com.example.quiz.repository.QuizRepository;
+
+@Service
+public class QuizServiceImpl implements QuizService{
+
+	/**
+	 * Repository:注入
+	 */
+	@Autowired
+	QuizRepository repository;
+	
+	@Override
+	public Iterable<Quiz> selectAll() {
+		return repository.findAll();
+	}
+	
+	@Override
+	public Optional<Quiz> selectOneById(Integer id) {
+		return repository.findById(id);
+	}
+	
+	@Override
+	public Optional<Quiz> selectOneRandom() {
+		// ランダムでidを取得する
+		Integer randId = repository.getRandomId();
+		if (randId == null) {
+			// 空のOptionalインスタンスを返す
+			return Optional.empty();
+		}
+		return repository.findById(randId);
+	}
+	
+	@Override
+	public Boolean checkQuiz(Integer id, Boolean myAnswer) {
+		Boolean check = false;
+		
+		// 対象のクイズを取得
+		Optional<Quiz> optQuiz = repository.findById(id);
+		
+		// 値存在チェック
+		if (optQuiz.isPresent()) {
+			Quiz quiz = optQuiz.get();
+			// クイズの解答チェック
+			if (quiz.getAnswer().equals(myAnswer)) {
+				check = true;
+			}
+		}
+		
+		return check;
+	}
+	
+	@Override
+	public void insertQuiz(Quiz quiz) {
+		repository.save(quiz);
+	}
+	
+	@Override
+	public void updateQuiz(Quiz quiz) {
+		repository.save(quiz);
+	}
+	
+	@Override
+	public void deleteQuizById(Integer id) {
+		repository.deleteById(id);
+	}
+}
