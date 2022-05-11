8e07798ffee57bd52326aa5594c08136a117b8c1
Yuta <y.sugiyama54@gmail.com>
2022-05-12 00:14:51 +0900

    クイズの登録機能の実装

--- src/main/java/com/example/quiz/controller/QuizController.java
+++ src/main/java/com/example/quiz/controller/QuizController.java
@@ -3,9 +3,13 @@ package com.example.quiz.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.quiz.entity.Quiz;
 import com.example.quiz.form.QuizForm;
@@ -53,6 +57,31 @@ public class QuizController {
 		model.addAttribute("title", "登録用フォーム");
 		
 		return "crud";
+	}
+	
+	/**
+	 * Quizデータを1件登録
+	 */
+	@PostMapping("/insert")
+	public String insert(@Validated QuizForm quizForm, BindingResult bindingResult, 
+			Model model, RedirectAttributes redirectAttributes) {
+		
+		// FormからEntityに詰め替え
+		Quiz quiz = new Quiz();
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		
+		// 入力チェック
+		if (!bindingResult.hasErrors()) {
+			service.insertQuiz(quiz);
+			redirectAttributes.addAttribute("complete", "登録が完了しました。");
+			return "redirect:/quiz";
+		} else {
+			return showList(quizForm, model);
+		}
+
 	}
 
 }
