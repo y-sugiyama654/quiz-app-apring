3491f06b7cd143530906a4effd6cef08f9a950d2
Yuta <y.sugiyama54@gmail.com>
2022-05-12 23:50:34 +0900

    クイズ更新機能の実装

--- src/main/java/com/example/quiz/controller/QuizController.java
+++ src/main/java/com/example/quiz/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.quiz.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -33,7 +36,6 @@ public class QuizController {
 	 */
 	@ModelAttribute
 	public QuizForm setUpForm() {
-		
 		QuizForm form = new QuizForm();
 		
 		form.setAnswer(true);
@@ -45,8 +47,7 @@ public class QuizController {
 	 * Quizの一覧を表示
 	 */
 	@GetMapping
-	public String showList(QuizForm quizForm, Model model) {
-		
+	public String showList(QuizForm quizForm, Model model) {		
 		quizForm.setNewQuiz(true);
 		
 		// クイズの一覧を取得する
@@ -64,8 +65,7 @@ public class QuizController {
 	 */
 	@PostMapping("/insert")
 	public String insert(@Validated QuizForm quizForm, BindingResult bindingResult, 
-			Model model, RedirectAttributes redirectAttributes) {
-		
+			Model model, RedirectAttributes redirectAttributes) {	
 		// FormからEntityに詰め替え
 		Quiz quiz = new Quiz();
 		quiz.setQuestion(quizForm.getQuestion());
@@ -82,6 +82,91 @@ public class QuizController {
 			return showList(quizForm, model);
 		}
 
+	}
+	
+	/*
+	 * Quizデータを1件取得しフォームに表示
+	 */
+	@GetMapping("/{id}")
+	public String showUpdate(QuizForm quizForm, @PathVariable Integer id, Model model) {
+		// Quizを取得
+		Optional<Quiz> quizOpt = service.selectOneById(id);
+		
+		// QuizFormへ詰め直し
+		Optional<QuizForm> quizFormOpt = quizOpt.map(t -> makeQuizForm(t));
+		
+		// QuizFormがnullでなければ中身を取り出す
+		if (quizFormOpt.isPresent()) {
+			quizForm = quizFormOpt.get();
+		}
+		
+		// 更新用のModelを作成
+		makeUpdateModel(quizForm, model);
+		
+		return "crud";
+	}
+	
+	/**
+	 * idをkeyにしてデータを更新
+	 */
+	@PostMapping("/update")
+	public String update(
+			@Validated QuizForm quizForm, 
+			BindingResult bindingResult, 
+			Model model, 
+			RedirectAttributes redirectAttributes) {
+		
+		// QuizFormからQuizに詰め直す
+		Quiz quiz = makeQuiz(quizForm);
+		
+		// 入力チェック
+		if (!bindingResult.hasErrors()) {
+			service.updateQuiz(quiz);
+			redirectAttributes.addFlashAttribute("complete", "更新が完了しました。");
+			
+			return "redirect:/quiz/" + quiz.getId();
+		} else {
+			makeUpdateModel(quizForm, model);
+			
+			return "crud";
+		}
+	}
+	
+	/**
+	 * 更新用のModelを作成
+	 */
+	private void makeUpdateModel(QuizForm quizForm, Model model) {
+		model.addAttribute("id", quizForm.getId());
+		quizForm.setNewQuiz(false);
+		model.addAttribute("quizForm", quizForm);
+		model.addAttribute("title", "更新用フォーム");
+	}
+	
+	/**
+	 * QuizFormからQuizへ詰め直す
+	 */
+	private Quiz makeQuiz(QuizForm quizForm) {
+		Quiz quiz = new Quiz();
+		quiz.setId(quizForm.getId());
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		
+		return quiz;
+	}
+	
+	/**
+	 * QuizからQuizFormへ詰め直す
+	 */
+	private QuizForm makeQuizForm(Quiz quiz) {
+		QuizForm form = new QuizForm();
+		form.setId(quiz.getId());
+		form.setQuestion(quiz.getQuestion());
+		form.setAnswer(quiz.getAnswer());
+		form.setAuthor(quiz.getAuthor());
+		form.setNewQuiz(false);
+		
+		return form;
 	}
 
 }
